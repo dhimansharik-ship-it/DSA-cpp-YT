567d455418119ffb9ddc1e65cd7b4a16f8a7f28c
Sharik Dhiman <dhimansharik@gmail.com>
2026-07-07 16:58:35 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Pattern 1.)"<<endl;
+int n = 4;
+int i ;
+for(i=0;i<n;i++){
+    for(int j=0;j<i+1;j++){
+        cout<<i+1;
+    }
+    cout<<endl;
+
+}
+cout<<endl;
+
+
+
+    return 0;
+
+}
