e3501a623a76ecd39048ad0a71965a0208fbb2e0
Sharik Dhiman <dhimansharik@gmail.com>
2026-07-21 23:24:21 +0530

    Add files via upload

---
+++ bubbleSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {4, 1, 5, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array before sorting: " << endl;
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Array after sorting: " << endl;
+    printArray(arr, n);
+
+    return 0;
+}
